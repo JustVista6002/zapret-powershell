7103c6c5bb2bd9e3f5a3440d6c9cde7c13d7db27
Seva <68725282+sevcator@users.noreply.github.com>
2024-12-25 16:18:00 +0700

    zapret.ps1

--- zapret.ps1
+++ zapret.ps1
@@ -1,119 +1,89 @@
 #
-# sevcator.github.io
+# 3=====D Idi nax PKH, He Tporau Hash Internet
 #
 Clear-Host
-
-# Modifable vars
 $folderPath = "C:\Windows\Zapret"
+$hostlist = "--hostlist-exclude=`"$folderPath\exclude.txt`" --hostlist-auto=`"$folderPath\autohostlist.txt`""
 $ARGS = "--wf-tcp=80,443 --wf-udp=80,443,50000-50099 "
-$ARGS += "--filter-tcp=80,443 --hostlist-auto=`"$folderPath\autohostlist.txt`" --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --hostlist-exclude=`"$folderPath\exclude.txt`" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls_clienthello_www_google_com.bin`" --dpi-desync-fake-quic=`"$folderPath\quic_initial_www_google_com.bin`" --new "
-$ARGS += "--filter-udp=50000-50099 --ipset=`"$folderPath\ipset-discord.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-any-protocol --dpi-desync-fake-tls=`"$folderPath\tls_clienthello_www_google_com.bin`" --dpi-desync-fake-quic=`"$folderPath\quic_initial_www_google_com.bin`" --new "
-$ARGS += "--filter-udp=80,443 --hostlist-auto=`"$folderPath\autohostlist.txt`" --hostlist-auto-fail-threshold=2 --hostlist-auto-fail-time=60 --hostlist-auto-retrans-threshold=2 --hostlist-exclude=`"$folderPath\exclude.txt`" --dpi-desync=fake,split2 --dpi-desync-repeats=11 --dpi-desync-fake-tls=`"$folderPath\tls_clienthello_www_google_com.bin`" --dpi-desync-fake-quic=`"$folderPath\quic_initial_www_google_com.bin`""
-
-Write-Host "  ______                         _   " -ForegroundColor Cyan
-Write-Host " |___  /                        | |  " -ForegroundColor Cyan
-Write-Host "    / /  __ _  _ __   _ __  ___ | |_ " -ForegroundColor Cyan
-Write-Host "   / /  / _` || '_ \ | '__|/ _ \| __|" -ForegroundColor Cyan
-Write-Host "  / /__| (_| || |_) || |  |  __/| |_ " -ForegroundColor Cyan
-Write-Host " /_____|\__,_|| .__/ |_|   \___| \__|" -ForegroundColor Cyan
-Write-Host "              | |                    " -ForegroundColor Cyan
-Write-Host "              |_|                    " -ForegroundColor Cyan
-Write-Host "** github.com/sevcator/zapret-powershell"
-Write-Host "** github.com/bol-van/zapret"
-Write-Host "** github.com/bol-van/zapret-win-bundle"
-Write-Host ""
-
+$ARGS += "--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" $hostist --new "
+$ARGS += "--filter-tcp=443 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" --new "
+$ARGS += "--filter-tcp=80 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" --new "
+$ARGS += "--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" $hostlist --new "
+$ARGS += "--filter-udp=443 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$folderPath\quic.txt`" --new "
+$ARGS += "--filter-udp=80 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$folderPath\quic.txt`" --new "
+$ARGS += "--filter-udp=80 --dpi-desync=fake --dpi-desync-repeats=11 $hostlist --new "
+$ARGS += "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $hostlist --new "
+$ARGS += "--filter-udp=50000-50099 --ipset=`"$folderPath\ipset-discord.txt`" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4"
+Write-Host "  ______                         _   "
+Write-Host " |___  /                        | |  "
+Write-Host "    / /  __ _  _ __   _ __  ___ | |_ "
+Write-Host "   / /  / _` || '_ \ | '__|/ _ \| __|"
+Write-Host "  / /__| (_| || |_) || |  |  __/| |_ "
+Write-Host " /_____|\__,_|| .__/ |_|   \___| \__|"
+Write-Host "              | |                    "
+Write-Host "              |_|                    "
+Write-Host "Easy installation of bol-van software"
 function Check-Admin {
     $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
     return $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
-
 if (-not (Check-Admin)) {
     Add-Type -AssemblyName System.Windows.Forms 
     [System.Windows.Forms.MessageBox]::Show("zapret-sevcator | Run as administrator rights!")
     return
 }
-
 $initialDirectory = Get-Location
-
 $version = [System.Environment]::OSVersion.Version
-
 $windows10Version = New-Object System.Version(10, 0)
-
 if ($version -gt $windows10Version) {
-    Write-Output "Windows version: $version"
+    Write-Output "- Windows version: $version"
 } else {
-    Write-Host "Your version of Windows is old!" -ForegroundColor White
+    Write-Host "- Your version of Windows is old!"
     return
 }
-
 function Check-ProcessorArchitecture {
     $processor = Get-WmiObject -Class Win32_Processor
     return $processor.AddressWidth -eq 64
 }
-
 if (Check-ProcessorArchitecture) {
-    Write-Host "CPU Architecture is 64-bit"
+    Write-Host "- CPU Architecture is 64-bit"
 } else {
-    Write-Host "CPU Architecture is not 64-bit"
+    Write-Host "- CPU Architecture is not 64-bit"
     return
 }
-
 if (Test-Path "$folderPath\uninstall.cmd") {
     & "$folderPath\uninstall.cmd" | Out-Null
-} else {
-    Write-Output "Hm... You don't using my zapret before"
 }
 
-$adapters = Get-NetAdapter | Where-Object { $_.Status -eq "Up" }
-foreach ($adapter in $adapters) {
-    $ipAddresses = (Get-NetIPAddress -InterfaceAlias $adapter.Name -AddressFamily IPv4).IPAddress
-
-    if ($ipAddresses -contains "127.0.0.1") {
-        Set-NetIPInterface -InterfaceAlias $adapter.Name -Dhcp Enabled
-        Write-Host "Enabled DHCP on adapter $($adapter.Name) because it had IP 127.0.0.1"
-    }
-    else {
-        Write-Host "Adapter $($adapter.Name) does not have IP 127.0.0.1"
-    }
-}
-
-Write-Host "Terminating processes"
-
+Write-Host "- Terminating processes"
 $processesToKill = @("GoodbyeDPI.exe", "winws.exe", "zapret.exe", "dnscrypt-proxy.exe")
-
 foreach ($process in $processesToKill) {
     try {
         Stop-Process -Name $process -Force -ErrorAction Stop
-        Write-Host "${process}: Killed"
     } catch {
         if ($_.Exception.Message -like "*not running*") {
-            Write-Host "${process}: Process not found" -ForegroundColor White
+
         } else {
             Write-Host ("{0}: {1}" -f $process, $_.Exception.Message) -ForegroundColor White
         }
     }
 }
 
-Write-Host "Checking services"
+Write-Host "- Removing services"
 $servicesToStop = @("zapret", "winws1", "goodbyedpi", "windivert", "windivert14", "dnscrypt-proxy")
 foreach ($service in $servicesToStop) {
     $serviceStatus = Get-Service -Name $service -ErrorAction SilentlyContinue
 
     if ($serviceStatus) {
-        Write-Host "${service}: Stopping"
-        # Stop the service and handle potential errors
         try {
             Stop-Service -Name $service -Force -ErrorAction Stop
-            Write-Host "${service}: Stopped successfully"
         } catch {
             Write-Host ("{0}: {1}" -f $service, $_.Exception.Message) -ForegroundColor Red
         }
         
         try {
             sc.exe delete $service -ErrorAction Stop | Out-Null
-            Write-Host "${service}: Deleted"
         } catch {
             Write-Host ("{0}: {1}" -f $service, $_.Exception.Message) -ForegroundColor Yellow
 	    Write-Host "If you have problems, try restart your machine!" -ForegroundColor Yellow
@@ -122,7 +92,6 @@ foreach ($service in $servicesToStop) {
         Write-Host "${service}: Not found" -ForegroundColor White
     }
 }
-
 if (Test-Path $folderPath) {
     $items = Get-ChildItem -Path $folderPath -Recurse
     $filesToRemove = $items | Where-Object { $_.PSIsContainer -eq $false -and $_.Extension -ne ".txt" }
@@ -136,28 +105,21 @@ if (Test-Path $folderPath) {
 } else {
     New-Item -Path $folderPath -ItemType Directory | Out-Null
 }
-
 if (-not (Test-Path $folderPath)) {
     New-Item -Path $folderPath -ItemType Directory | Out-Null
 }
 
 $exclusionPath = "$folderPath\winws.exe"
-
+Write-Host "- Adding exclusion"
 if (-not (Test-Path $exclusionPath)) {
     New-Item -Path $exclusionPath -ItemType File | Out-Null
-    Write-Host "${exclusionPath}: Created"
-} else {
-    Write-Host "${exclusionPath}: Already exists" -ForegroundColor Yellow
 }
-
 try {
     Add-MpPreference -ExclusionPath $exclusionPath
-    Write-Host "${exclusionPath}: Added to exclusions in Windows Defender"
     Start-Sleep -Seconds 5
-} catch {
-    Write-Host ("${exclusionPath}: Error to add exclusion - {0}" -f $_.Exception.Message) -ForegroundColor Red
 }
 
+Write-Host "- Downloading files"
 $files = @(
     @{Url = "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert.dll"; Name = "WinDivert.dll"},
     @{Url = "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/WinDivert64.sys"; Name = "WinDivert64.sys"},
@@ -165,40 +127,30 @@ $files = @(
     @{Url = "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/winws.exe"; Name = "winws.exe"},
     @{Url = "https://raw.githubusercontent.com/bol-van/zapret-win-bundle/refs/heads/master/zapret-winws/ipset-discord.txt"; Name = "ipset-discord.txt"},
     @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/autohostlist.txt"; Name = "autohostlist.txt"},
+    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/google.txt"; Name = "google.txt"},
     @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/exclude.txt"; Name = "exclude.txt"},
-    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/tls_clienthello_www_google_com.bin"; Name = "tls_clienthello_www_google_com.bin"}
-    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/quic_initial_www_google_com.bin"; Name = "quic_initial_www_google_com.bin"}
+    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/tls_clienthello_www_google_com.bin"; Name = "tls.bin"}
+    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/quic_initial_www_google_com.bin"; Name = "quic.bin"}
     @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/uninstall.cmd"; Name = "uninstall.cmd"}
 )
-
 foreach ($file in $files) {
     try {
         Invoke-WebRequest -Uri $file.Url -OutFile "$folderPath\$($file.Name)" -ErrorAction Stop | Out-Null
-        Write-Host "$($file.Name): Downloaded"
     } catch {
         Write-Host ("{0}: {1}" -f $($file.Name), $_.Exception.Message) -ForegroundColor Red
     }
 }
 
 Set-Location $folderPath | Out-Null
-
+Write-Host "- Creating service"
 try {
     sc.exe create winws1 binPath= "`"$folderPath\winws.exe $ARGS`"" DisplayName= "zapret DPI bypass" start= auto | Out-Null
     sc.exe start winws1 | Out-Null
-    Write-Host "Service winws1 created and started!"
 } catch {
     Write-Host ("Failed to create or start service: {0}" -f $_.Exception.Message) -ForegroundColor Red
 }
 
-Write-Host ""
-Write-Host "Done! Now enjoy."
-Write-Host "To remove Zapret, run script located in $folderPath\uninstall.cmd as administrator!" -ForegroundColor Yellow
-Write-Host ""
-Write-Host "!!! Problems with accessing on your browser? Use Private DNS on Settings your browser."
-Write-Host ""
-Write-Host "!!! If Discord stucks on 'Checking for updates...' terminate process and open again" -ForegroundColor Gray
-Write-Host "!!! On some providers this DPI Bypass may not working! Uninstall and use VPN or something else (try other zapret)" -ForegroundColor Gray
-Write-Host ""
-Write-Host "Follow me ** sevcator.github.io & sevcator.t.me"
-Write-Host ""
+Write-Host "*** sevcator.t.me / sevcator.github.io ***"
+Write-Host "- Done!"
+Write-Host "- To remove Zapret, run script located in $folderPath\uninstall.cmd as administrator!"
 Set-Location $initialDirectory
